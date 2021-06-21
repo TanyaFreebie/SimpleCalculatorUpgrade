4c630585e87ad3e306257c90717a83cf75f0402f
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-06-21 21:07:19 +0300

    first case

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double firstValue;
+	double secondValue;
+	double answer;
+
+	firstValue = 22;
+	secondValue =7;
+
+	String operator = "+";
+
+	System.out.println(doArithmetic(firstValue, secondValue, operator));
+
+    }
+    public static double doArithmetic(double firstNumber, double secondNumber, String operator){
+    	double  answer = 0;
+
+		switch (operator) {
+			case "+":
+				answer = firstNumber + secondNumber;
+				return answer;
+			case "-":
+				return answer;
+			case "*":
+				return answer;
+			case "/":
+				answer = firstNumber / secondNumber;
+				return answer;
+			default:
+				System.out.println("invalid input");
+		}
+		return answer;
+	}
+}
