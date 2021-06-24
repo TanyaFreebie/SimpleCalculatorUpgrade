6b00243775c2671ba75dcacc23c256073fd5c633
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-06-24 20:46:12 +0300

    hometask finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,10 @@ public class Main {
 	firstValue = 22;
 	secondValue =7;
 
-	String operator = "+";
+	String operator = "max";
 
 	System.out.println(doArithmetic(firstValue, secondValue, operator));
-
+	System.out.println("answer hometask:" + doMaxMin(firstValue, secondValue, operator));
     }
     public static double doArithmetic(double firstNumber, double secondNumber, String operator){
     	double  answer = 0;
@@ -37,5 +37,20 @@ public class Main {
 
 	//create a method that checks two numbers and uses switch to
 	//return max, min
+	public static double doMaxMin(double NumberOne, double NumberTwo, String operator){
+    	double answer = 0;
+    	switch (operator){
+			case "min":
+				answer = Math.min(NumberOne, NumberTwo);
+				return answer;
+			case "max":
+				answer = Math.max(NumberOne, NumberTwo);
+				return answer;
+
+			default:
+				System.out.println("Unexpected value: " + operator);
+		}
+		return answer;
+	}
 
 }
